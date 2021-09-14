3677371a93c8d50573cda3f9eb07409ab18893cc
abberadhi <abbe.radhi11@gmail.com>
2021-09-14 18:36:38 +0200

    own component for nav

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React, { useState } from 'react';
+import Nav from './components/nav';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faFileWord } from '@fortawesome/free-solid-svg-icons'
-
-
-
 
 function App () {
     const [myEditor, setMyEditor] = useState(null);
                 // onClick={ () => console.log(myEditor.getData())}
     return (
         <div className="App">
-            <nav className="navbar">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a href="" className="nav-link">
-                            {/* <span class="link-text">Abbe</span> */}
-                            <FontAwesomeIcon size="lg" icon={faPlus} />
-                        </a>
-                    </li>
-
-                    <li className="nav-item">
-                        <a href="" className="nav-link">
-                            {/* <span class="link-text">Abbe</span> */}
-                            <FontAwesomeIcon size="lg" icon={faFileWord} />
-                        </a>
-                    </li>
-
-                                        <li className="nav-item">
-                        <a href="" className="nav-link">
-                            {/* <span class="link-text">Abbe</span> */}
-                            <FontAwesomeIcon size="lg" icon={faFileWord} />
-                        </a>
-                    </li>
-                </ul>
-            </nav>        
+            <Nav></Nav>
             <CKEditor
                 editor={ ClassicEditor }
                 data="<p>Hello from CKEditor 5!</p>"
---
+++ src/components/nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faFileWord } from '@fortawesome/free-solid-svg-icons'
+
+function Nav () {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a href="" className="nav-link">
+                        {/* <span class="link-text">Abbe</span> */}
+                        <FontAwesomeIcon size="lg" icon={faPlus} />
+                    </a>
+                </li>
+
+                <li className="nav-item">
+                    <a href="" className="nav-link">
+                        {/* <span class="link-text">Abbe</span> */}
+                        <FontAwesomeIcon size="lg" icon={faFileWord} />
+                    </a>
+                </li>
+
+                                    <li className="nav-item">
+                    <a href="" className="nav-link">
+                        {/* <span class="link-text">Abbe</span> */}
+                        <FontAwesomeIcon size="lg" icon={faFileWord} />
+                    </a>
+                </li>
+            </ul>
+        </nav>        
+    );
+}
+
+export default Nav;
