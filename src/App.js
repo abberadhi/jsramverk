e3abc079082aea3e22a6713aa251dabcd65c1fbb
abberadhi <abbe.radhi11@gmail.com>
2021-09-14 18:18:20 +0200

    vertical nav

--- src/App.js
+++ src/App.js
@@ -2,39 +2,51 @@ import React, { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faFileWord } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
 function App () {
-  const [myEditor, setMyEditor] = useState(null);
+    const [myEditor, setMyEditor] = useState(null);
                 // onClick={ () => console.log(myEditor.getData())}
+    return (
+        <div className="App">
+            <nav className="navbar">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a href="" className="nav-link">
+                            {/* <span class="link-text">Abbe</span> */}
+                            <FontAwesomeIcon size="lg" icon={faPlus} />
+                        </a>
+                    </li>
 
-  return (
-    <div className="App">
+                    <li className="nav-item">
+                        <a href="" className="nav-link">
+                            {/* <span class="link-text">Abbe</span> */}
+                            <FontAwesomeIcon size="lg" icon={faFileWord} />
+                        </a>
+                    </li>
 
-        <nav className="navbar">
-            <ul className="navbar-nav">
-                <li class="nav-item">
-                    <a href="" className="nav-link">
-                        {/* <span class="link-text">Abbe</span> */}
-                        <FontAwesomeIcon icon={faPlus} />
-                        Add
-                    </a>
-                </li>
-            </ul>
-        </nav>        
-        <CKEditor
-            editor={ ClassicEditor }
-            data="<p>Hello from CKEditor 5!</p>"
-            onReady={ editor => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );
-                setMyEditor(editor);
-            } }
-        />
-    </div>
-);
+                                        <li className="nav-item">
+                        <a href="" className="nav-link">
+                            {/* <span class="link-text">Abbe</span> */}
+                            <FontAwesomeIcon size="lg" icon={faFileWord} />
+                        </a>
+                    </li>
+                </ul>
+            </nav>        
+            <CKEditor
+                editor={ ClassicEditor }
+                data="<p>Hello from CKEditor 5!</p>"
+                onReady={ editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                    setMyEditor(editor);
+                } }
+            />
+        </div>
+    );
 }
 
 export default App;
