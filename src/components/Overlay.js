8d7afb40727fed454eb954ada5f6b4c9dcbad485
abberadhi <abbe.radhi11@gmail.com>
2021-09-18 22:00:35 +0200

    added input field and styled the overlay

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Overlay (data) {
     return (
         <div className="overlay" onClick={() => console.log("Remove Overlay")}>
-            <p>Content</p>
+            <div class="left">
+            <p style={{display: "inline-block"}}>Title: </p>
+            <input type="text" className="input"></input>
+            <p className="created">Created: 2020-09-18</p>
+            </div>
+
+            <p className="edited">Last updated: 5 min ago</p>
         </div>
     );
 }
