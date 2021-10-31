d4bf6fe4d54a029330e1a469e0b2ed05d33b6732
abberadhi <abbe.radhi11@gmail.com>
2021-10-31 19:35:53 +0100

    login on correct credentials

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import DateUtils from '../utils/DateUtils';
 import Loader from '../components/Loader';
 import url from '../utils/url';
@@ -8,14 +8,18 @@ import auth from '../utils/auth';
 
 
 function SignIn (props) {
+    let history = useHistory();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [errMsg, setErrMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
+    
     
     function handleSubmit(event) {
         event.preventDefault();
-        
-
+        setLoading(true);
+        console.log("this1", this);
+        auth.login(email, password, setLoading, setErrMsg).then(() => history.push(url("/" )));
     }
 
     function validateForm() {
@@ -25,26 +29,28 @@ function SignIn (props) {
     return (
         <div className="signin">
             <h1>Sign in</h1>
-            <div className="errMsg"><h4>Error</h4>
-            <p>Something happened</p>
-            </div>
 
-            <form onSubmit={handleSubmit}> 
-                <div className="container">
-                    <label for="uname"><b>Email</b></label>
-                    <input autoFocus onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter Username" name="uname" required></input>
-                    
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" name="psw" required></input>
+            {!loading ? (<div>
+                {errMsg ? <div className="errMsg"><h4>Error</h4>
+                <p>{errMsg}</p>
+                </div> : null}
+                
 
-                    <button type="submit" disabled={!validateForm()}>Login</button>
-                </div>
-                <div class="container">
-                    {/* <button type="button" class="cancelbtn">Cancel</button> */}
-                    <span class="psw">Not registered? <a href="#">Click here</a></span>
-                </div>
-            </form>            
+                <form onSubmit={handleSubmit}> 
+                    <div className="container">
+                        <label for="uname"><b>Email</b></label>
+                        <input autoFocus onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter Username" name="uname" required></input>
+                        
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" name="psw" required></input>
 
+                        <button type="submit" disabled={!validateForm()}>Login</button>
+                    </div>
+                    <div class="container">
+                        <span class="psw">Not registered? <a href="#">Click here</a></span>
+                    </div>
+                </form>            
+            </div>) : <Loader></Loader>}
         </div>
     );
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,50 @@
+import axios from 'axios';
 class Auth {
     constructor() {
+        this.email = false;
+        this.token = false;
+        this.id = false;
         this.authenticated = false;
     }
 
-    login() {
-        this.authenticated = true;
+    login(email, password, setLoading, setErrMsg) {
+
+        return new Promise((resolve, reject) => {
+            axios({
+                url: "/graphql",
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                data: {
+                    query: `mutation { userLogin(email: "${email}", password: "${password}") {  id, email, token, msg }}`}
+            }).then((response => {
+                let res = response.data.data.userLogin;
+                setLoading(false);
+    
+                if (res.msg) { 
+                    setErrMsg(res.msg)
+                    reject()
+                    return false;
+                }
+    
+                this.authenticated = true;
+                this.email = res.email;
+                this.id = res.id;
+                this.token = res.token;
+
+                resolve()
+            }))
+          });
+
+
     }
 
     logout() {
+        this.email = false;
+        this.token = false;
+        this.id = false;
         this.authenticated = false;
     }
 
@@ -16,4 +53,4 @@ class Auth {
     }
 }
 
-module.exports = new Auth();
+export default new Auth();
